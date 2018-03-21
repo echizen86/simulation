6dc157cb12888c61899855ff158fd771310477e7
Jose Castillo <jose860304@gmail.com>
2018-03-21 12:38:21 -0400

    primeros pasos en seguridad de spring, nada de dominio aun

---
+++ simulation/src/main/java/com/example/demo/controllers/AccountController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+	@RequestMapping(value = "/public/{accountNumber}")
+	public String getPublicAccount(@PathVariable final int accountNumber) {
+		return "Public Account # " + accountNumber;
+	}
+	
+	@RequestMapping(value = "/private/{accountNumber}")
+	public String getPrivateAccount(@PathVariable final int accountNumber) {
+		return "Private Account # " + accountNumber;
+	}
+	
+	@RequestMapping(value = "/private/admin/{accountNumber}")
+	public String getPrivateAccountAdmin(@PathVariable final int accountNumber) {
+		return "Private Account Admin # " + accountNumber;
+	}
+}
